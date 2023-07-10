00c7df681d35edcb3446a1e2aa8f0e85a7874adb
Saif Ali <kattan.saifali@gmail.com>
2023-07-10 23:49:29 +0200

    exercis  completed

---
+++ 2-Even-or-Odd.py
@@ -0,0 +1,26 @@
+
+# Create a function that takes an integer as an argument and returns 
+# "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(number):
+    
+    if number %2 == 0:
+#         print (number,"Even")
+        return 'Even'
+        
+    else:
+#         print(number,"Odd")
+        return 'Odd'
+
+
+#  this is another solution 
+
+def even_or_odd(number):
+	return 'Odd' if number % 2 else 'Even'
+# using ternary operator to check whether the remainder of dividing by two with no decimal part equals
+# zero, which means it's an even number
+# ; otherwise its odd
+# باستخدام عامل التشغيل الثلاثي للتحقق مما إذا كان باقي القسمة على اثنين بدون جزء عشري يساوي
+# # صفر ، مما يعني أنه رقم زوجي
+# # ؛ خلاف ذلك من الغريب
+
