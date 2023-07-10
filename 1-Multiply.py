0e658845ba1da92f15d73a02f8b8fe4c82afe091
Saif Ali <kattan.saifali@gmail.com>
2023-07-10 12:38:45 +0200

    exercis  completed

---
+++ 1-Multiply.py
@@ -0,0 +1,11 @@
+
+# This code does not execute properly. Try to figure out why.
+
+def multiply(a, b):
+    return a * b
+
+#  this is another solution 
+
+def multiply(a, b):
+    code = a * b
+    return code 
