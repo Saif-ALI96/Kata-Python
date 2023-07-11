67dd076cf235dbfb018f32662627a2a495a99e8c
Saif Ali <kattan.saifali@gmail.com>
2023-07-11 10:29:42 +0200

    exercis  completed

---
+++ 5-RevesedString.py
@@ -0,0 +1,15 @@
+#  Reversed String 
+
+# Complete the solution so that it reverses the string passed into it.
+
+def solution(string):
+
+    return string[::-1]
+
+#  c'est comprender un peu mieux la reponse 
+# print (solution("hello")) # Output: "olleh"
+
+
+
+
+#  this is another solution 
