de902f23964ef33c76293c1f0c7a9a82f37312af
Saif Ali <kattan.saifali@gmail.com>
2023-07-12 22:14:31 +0200

    exercis  completed

---
+++ 7-ReturnNegative.py
@@ -0,0 +1,19 @@
+
+
+#  Return Negative
+
+# In this simple assignment you are given a number and have to make it negative.
+#  But maybe the number is already negative?
+
+def make_negative( number ):
+    pass
+    return -abs(number)
+
+#  abs() retrun le number vers le positive et le 
+# -abs() retrun le number vers le negative 
+
+
+#  another solution 
+
+def make_negative( number ):
+    return -number if number>0 else number
