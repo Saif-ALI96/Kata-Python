6811e4454ae7eb8b4c1391ec546067f3a5a89bd0
Saif Ali <kattan.saifali@gmail.com>
2023-07-16 11:55:44 +0200

    exercis completed

---
+++ 18-stringDouble.py
@@ -0,0 +1,18 @@
+# You Can't Code Under Pressure #1
+
+
+'''
+Code as fast as you can! You need to double the integer and return it.
+Here's one way:
+```python
+def double_integer(num):
+result = num * 2
+return result
+
+'''
+
+def double_integer(i):
+    pass # Double the integer and return it!
+    return i*2
+print('here is the double of 3 is : ' + str(double_integer(3)))
+#   -> here is the double of 3 is : 6
