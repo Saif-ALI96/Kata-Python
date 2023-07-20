d70dc24606dcf8177487687b7e304de8df97732b
Saif Ali <kattan.saifali@gmail.com>
2023-07-20 21:56:25 +0200

    update the file

---
+++ 25-abbreviateTwoWordName.py
@@ -0,0 +1,36 @@
+
+# Abbreviate a Two Word Name
+
+'''
+Write a function to convert a name into initials. 
+This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+'''
+
+
+def abbrev_name(name):
+        
+    initial  = '.'.join([loop[0].upper() for loop in name.split(' ')])
+    return initial
+print(abbrev_name('hello world'))
+
+
+
+#  another solution
+# 1
+def abbrev_name(name):
+    # Convert name to uppercase
+    uppername = name.upper()
+    # Split string
+    seprate_words = uppername.split()
+    # Get first letters
+    letters = [word[0] for word in seprate_words]
+    # Return + join letters with a seperating "."
+    return (".".join(letters))
