55beda5c5ed37d6666f52a27bc8b707f17a0a6dc
Saif Ali <kattan.saifali@gmail.com>
2023-07-24 21:02:45 +0200

    update the file

---
+++ 10-removeFirstandLastCharacter.py
@@ -0,0 +1,23 @@
+# 
+# Remove First and Last Character
+
+# It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string.
+#  You don't have to worry with strings with less than two characters.
+
+def remove_char(s):
+    #your code here
+    return s[1:-1]
+
+print(remove_char('hello world'))  # sortie -----> ello worl
+# pour verser le string 
+
+
+# another solution 
+
+remove_char=lambda s: s[1:-1]
+
+
+# another solution 
+
+def remove_char(s):
+    return s[1:len(s)-1]
