76900f03d15d6fb7f878d041ed85f7eba99abc7f
Saif Ali <kattan.saifali@gmail.com>
2023-09-06 22:48:20 +0200

    update the file

--- 9-StringRepeat.py
+++ 9-StringRepeat.py
@@ -1,8 +1,8 @@
 
 # String repeat
-
+'''
 # Write a function that accepts an integer n and a string s as parameters,
-#  and returns a string of s repeated exactly n times.
+#  and returns a string of s repeated exactly n times.'''
 
 
 def repeat_str(repeat, string):
